3ca799ff070a660228a62d6aae691ccc44e5cd11
backy22 <tsubakino@backys-MacBook-Pro.local>
2020-02-20 22:02:48 -0500

    icon fix

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import weblink from "../img/weblink.svg";
-import githublink from "../img/githublink.svg";
 import givetakeThumbnail from "../img/givetake-thumbnail.png";
 import seanThumbnail from "../img/sean-thumbnail.png";
 import tsubakiThumbnail from "../img/tsubaki-thumbnail.png";
@@ -23,7 +21,9 @@ const Project = props => {
 const WebLink = props => {
   if (props.project.weblink){
     return (
-      <a href={props.project.weblink} target="blank"><img className="linkicon" src={weblink} alt="web link icon"/></a>
+      <a href={props.project.weblink} target="blank">
+        <svg className="linkicon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 15.79 15.79"><defs></defs><title>Asset 2</title><path className="cls-1" d="M15.07,0H9.33a.72.72,0,1,0,0,1.44h4L6,8.82a.72.72,0,0,0,1,1l7.39-7.39v4a.72.72,0,1,0,1.43,0V.72A.72.72,0,0,0,15.07,0Z"/><path className="cls-1" d="M12.2,7.18a.72.72,0,0,0-.72.72v6.46h-10v-10H7.9a.72.72,0,0,0,0-1.44H.72A.72.72,0,0,0,0,3.59V15.07a.72.72,0,0,0,.72.72H12.2a.72.72,0,0,0,.72-.72V7.9A.72.72,0,0,0,12.2,7.18Z"/></svg>
+      </a>
     );
   }else{
     return null;
@@ -33,7 +33,9 @@ const WebLink = props => {
 const GithubLink = props => {
   if (props.project.githublink){
     return (
-      <a href={props.project.githublink} target="blank"><img className="linkicon" src={githublink} alt="github icon"/></a>
+      <a href={props.project.githublink} target="blank">
+        <svg className="linkicon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 34.42 33.57"><defs></defs><title>Asset 19</title><path className="cls-1" d="M17.21,0a17.21,17.21,0,0,0-5.44,33.54c.86.16,1.13-.35,1.13-.81s0-1.55,0-3c-4.78,1-5.78-2.27-5.78-2.27A4.55,4.55,0,0,0,5.21,25c-1.56-1.07.12-1,.12-1A3.61,3.61,0,0,1,8,25.69a3.67,3.67,0,0,0,5,1.43,3.66,3.66,0,0,1,1.09-2.3c-3.82-.44-7.84-1.91-7.84-8.51A6.67,6.67,0,0,1,8,11.69a6.14,6.14,0,0,1,.17-4.55S9.62,6.68,12.9,8.9a16.3,16.3,0,0,1,8.62,0c3.29-2.22,4.73-1.76,4.73-1.76a6.14,6.14,0,0,1,.17,4.55,6.67,6.67,0,0,1,1.77,4.62c0,6.61-4,8.07-7.86,8.49A4.08,4.08,0,0,1,21.51,28v4.77c0,.46.28,1,1.15.8A17.21,17.21,0,0,0,17.21,0Z"/></svg>
+      </a>
     );
   }else{
     return null;
