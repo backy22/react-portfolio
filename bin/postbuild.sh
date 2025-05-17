50225a33f06ba3da0b52750b72f80cc89d9a7991
Aya Tsubakino <tsubakino@Ayas-MacBook-Air.local>
2025-05-17 22:20:19 +0900

    update deploy-manifest.json

--- bin/postbuild.sh
+++ bin/postbuild.sh
@@ -2,12 +2,17 @@
 
 rm -rf ./.amplify-hosting
 
-mkdir -p ./.amplify-hosting/compute
+mkdir -p ./.amplify-hosting/compute/default/src
 mkdir -p ./.amplify-hosting/static
 
-cp -r ./dist/server ./.amplify-hosting/compute/default
+# Copy server files
+cp -r ./dist/server/src/* ./.amplify-hosting/compute/default/src/
 cp -r ./node_modules ./.amplify-hosting/compute/default/node_modules
-cp -r ./dist ./.amplify-hosting/static
-cp .env ./.amplify-hosting/compute/default/.env
 
+# Copy static files (frontend)
+cp -r ./dist/* ./.amplify-hosting/static/
+
+# Copy environment and config files
+cp .env ./.amplify-hosting/compute/default/.env
+cp package.json ./.amplify-hosting/compute/default/package.json
 cp deploy-manifest.json ./.amplify-hosting/deploy-manifest.json 
