322a3dab2ad1c0e8c1e0411fd8a34cacc958b8c5
Aya Tsubakino <tsubakino@Ayas-MacBook-Air.local>
2025-05-19 13:44:54 +0900

    update compute resouece

--- postbuild.sh
+++ postbuild.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
 # Create the amplify-hosting directory structure
-mkdir -p .amplify-hosting/compute/default/client
-mkdir -p .amplify-hosting/compute/default/server
+mkdir -p .amplify-hosting/compute/default
 mkdir -p .amplify-hosting/static
 
 # Copy the client build
-cp -r dist/client/* .amplify-hosting/compute/default/client/
 cp -r dist/client/* .amplify-hosting/static/
 
 # Copy the server build
-cp -r dist/server/* .amplify-hosting/compute/default/server/
+cp -r dist/server/* .amplify-hosting/compute/default/
 
 # Copy package.json
 cp package.json .amplify-hosting/compute/default/
